d95aaf832a7ab3bcf5923fe8acf1dfb1911b9c3f
rubylouvre <1669866773@qq.com>
2017-04-12 01:21:30 +0800

    让其可用

--- index2.js
+++ index2.js
@@ -1,104 +1,111 @@
-var str = `<div {ddd} id={dd}></div>`
+var str = `<div class="ddd" {ddd} id={dd}></div>`
 
-function lexer(html, opts) {
-    let string = html
-    let tokens = []
-    var stopIndex = 999999
-    while (string) {
-        if (--stopIndex === 0) {
+function lexer(string, opts) {
+    var tokens = []
+    var breakIndex = 9
+    var stack = []
+    stack.last = function() {
+        return stack[stack.length - 1]
+    }
+    var ret = []
+
+    function addNode(node) {
+        var p = stack.last()
+        if (p && p.children) {
+            p.children.push(node)
+        } else {
+            ret.push(node)
+        }
+    }
+
+    do {
+        if (--breakIndex === 0) {
             break
         }
-        if (string.indexOf("</") === 0) { //处理闭标签
-            const match = string.match(REGEXP.endTag)
-            if (!match)
-                continue
-            string = string.substring(match[0].length)
-            tokens.push({
-                tag: match[1],
-                type: 'tag-end',
-            })
+        console.log(string)
+        var arr = getCloseTag(string)
+        if (arr) {
+            console.log(arr)
+            string = string.replace(arr[0], '')
+            stack.pop()
+
+            continue
+        }
+        var arr = getOpenTag(string)
+        if (arr) {
+            string = string.replace(arr[0], '')
+            var node = arr[1]
+            addNode(node)
+            if (!node.isVoidTag) {
+                stack.push(node)
+            }
             continue
         }
 
-        if (string.indexOf("<") === 0) { //处理开标签
-            if (string.indexOf('<!--') === 0) { //处理注释标签
-                const i = string.indexOf('-->', 4)
-                if (i === -1) {
-                    throw '注释节点没有闭合'
-                }
-                tokens.push({
-                    tag: '#comment',
-                    type: 'tag-empty',
-                    text: string.slice(4, i)
-                })
-                string = string.slice(i + 3)
-                continue
-            }
-            //<div id=''/> <p>
-            const match = string.match(/<(\w[^\s\/\>]+)/)
-            if (match) {
-                const tag = match[1]
 
-                string = string.slice(match[0].length)
-                if (string.charAt(0) === '/' || string.charAt(0) == '>')
-                    const attributes = getAttributes(match[2])
-                        //   string = string.substring(match[0].length)
-                if (TAG.special[tag]) {
-                    var v = string.indexOf('</' + tag + '>')
-                    tokens.push({
-                        tag: tag,
-                        type: 'tag-empty',
-                        props: attributes,
-                        children: [{
-                            type: "#text",
-                            text: string.slice(0, v)
-                        }]
-                    })
-                    string = string.slice(v + 2 + tag.length + 1)
-                    continue
+
+    } while (string.length);
+
+    console.log(ret)
+
+
+}
+lexer(str)
+
+function parseCode(string) {
+    var state = 'start',
+        word = '',
+        needReset = false,
+        braceIndex = 1,
+        quote
+
+    for (var i = 0, n = string.length; i < n; i++) {
+        var c = string[i]
+
+        if (quote) {
+            if (c === quote) {
+                quote = ''
+            }
+        } else {
+            if (c === '"' || c === "'") {
+                word = ''
+                quote = c
+            } else if (c === '{') {
+                word = ''
+                braceIndex++
+            } else if (c === '}') {
+                word = ''
+                braceIndex--
+                if (braceIndex === 0) {
+                    return [string.slice(0, i), {
+                        type: 'jsx',
+                        value: string.slice(0, i)
+                    }]
+                }
+            } else if (c === '[' || c === ']' || c === '(' || c === ')' || c === ',') {
+                word = ''
+            } else if (c === '<') {
+                if (word === '' || word === 'return' || word.slice(-2) == '=>') {
+                    if (/\<\w/.test(string.slice(i))) {
+
+                    }
+                    ok = true
                 }
 
-                const type = !!MAKER.empty[tag] || match[0].slice(-2) === '/>' ? 'tag-empty' : 'tag-start'
-                tokens.push({
-                    tag: tag,
-                    type: type,
-                    children: [],
-                    props: attributes
-                })
-                continue
-            }
-        }
-        var text = ''
-        do {
-            const index = string.indexOf('<')
-            if (index === 0) {
-                text += string.slice(0, 1)
-                string = string.slice(1)
-            } else if (index === -1) {
-                text += string
-                string = ''
-                break
-            } else {
-                text += string.slice(0, index)
-                string = string.slice(index)
-                break
-            }
-        } while (string.length);
-        if (opts.skipEmptyNode ? /\S/.test(text) : text) {
-            var last = tokens[tokens.length - 1]
-            if (last && last.type === 'text') {
-                last.text += text
-            } else {
-                tokens.push({
-                    type: "text",
-                    text: text
-                })
+            } else if (c !== '') {
+                if (needReset) {
+                    word = c
+                    needReset = false
+                } else {
+                    word += c
+                }
+            } else if (c === ' ') {
+                needReset = true
             }
         }
     }
-    return tokens
-}
 
+}
 
 function getCloseTag(string) {
     if (string.indexOf("</") === 0) {
@@ -106,7 +113,7 @@ function getCloseTag(string) {
         if (match) {
             var tag = match[1]
             string = string.slice(3 + tag.length)
-            return [string, {
+            return [match[0], {
                 type: 'closeTag',
                 value: tag
             }]
@@ -117,87 +124,121 @@ function getCloseTag(string) {
 
 function getOpenTag(string) {
     if (string.indexOf("<") === 0) {
-        var match = string.match(/\<(\w[\s\/\>]*)/)
+
+        var i = str.indexOf('<!--') //处理注释节点
+        if (i === 0) {
+            var l = str.indexOf('-->')
+            if (l === -1) {
+                thow('注释节点没有闭合 ' + string.slice(0, 100))
+            }
+
+            var node = {
+                type: '#comment',
+                children: string.slice(4, i)
+            }
+            return [string.slice(0, i + 3), node]
+        }
+        var match = string.match(/\<(\w[^\s\/\>]*)/)
         if (match) {
-            var tag = match[1]
-            string = string.slice(1 + tag.length)
-            var left = getAttr(string)
+            var left = match[0]
+            var node = {
+                type: match[1],
+                props: {},
+                children: []
+            }
 
+            //处理属性
+            var arr = getAttrs(string.replace(left, ''))
+            if (arr) {
+                node.props = arr[1]
+                left += arr[0]
+            }
 
-            return [string, {
-                type: 'openTag',
-                value: tag
-            }]
+            var right = string.replace(left, '')
+            if (right[0] === '>') {
+                left += '>'
+            } else if (right.slice(0, 2) === '/>') {
+                left += '/>'
+                node.isVoidTag = true
+            }
+
+            return [left, node]
         }
     }
 }
+var breakAttr = 0
 
 function getAttrs(string) {
     var state = 'AttrNameOrJSX',
         attrName = '',
         attrValue = '',
         quote,
-        attrs = {}
-    breakLabel:
-        for (var i = 0, n = string.length; i < n; i++) {
-            var c = string[i]
-            switch (state) {
-                case 'AttrNameOrJSX':
-                    if (c === '/' || c === '>') {
-                        break breakLabel
-                    }
-                    if (c === ' ') {
-                        if (attrName) {
-                            state = 'AttrEqual'
-                        }
+        props = {}
 
-                    } else if (c === '=') {
-                        state = 'AttrQuoteOrJSX'
-                    } else if (c === '{') {
-                        state = 'SpreadJSX'
-                    } else {
-                        attrName += c
+    for (var i = 0, n = string.length; i < n; i++) {
+        var c = string[i]
+        switch (state) {
+            case 'AttrNameOrJSX':
+                if (c === '/' || c === '>') {
+                    console.log(string.slice(0, i))
+                    return [string.slice(0, i), props]
+                }
+                if (c === ' ') {
+                    if (attrName) {
+                        state = 'AttrEqual'
                     }
-                    break
-                case 'AttrEqual':
-                    if (c === '=') {
-                        state = 'AttrQuoteOrJSX'
+                } else if (c === '=') {
+                    if (!attrName) {
+                        throw '必须指定属性名'
                     }
-                    break
-                case 'AttrQuoteOrJSX':
-                    if (c === '"' || c === "'") {
-                        quote = c
-                        state = 'AttrValue'
-                    } else if (c === '{') {
-                        state = 'JSX'
-                    }
-                    break
-                case 'AttrValue':
-                    if (c !== quote) {
-                        attrValue += c
-                    } else if (c === quote) {
-                        attrs[attrName] = attrValue
-                        attrName = ''
-                        attrValue = ''
-                        flag == 'AttrNameOrJSX'
-                    }
-                    break
-                case 'SpreadJSX':
-                case 'JSX':
-                    var arr = parserJSX(string.slice(i), true)
-                    i += attr[0].length + 1
-                    if (state === 'SpreadJSX') {
-                        props[SpreadJSX] = attr[1]
-                    } else {
-                        props[attrName] = attr[1]
-                    }
-                    break
+                    state = 'AttrQuoteOrJSX'
+                } else if (c === '{') {
+                    state = 'SpreadJSX'
+                } else {
+                    attrName += c
+                }
+                break
+            case 'AttrEqual':
+                if (c === '=') {
+                    state = 'AttrQuoteOrJSX'
+                }
+                break
+            case 'AttrQuoteOrJSX':
+                if (c === '"' || c === "'") {
+                    quote = c
+                    state = 'AttrValue'
+                } else if (c === '{') {
+                    state = 'JSX'
+                }
+                break
+            case 'AttrValue':
+                if (c !== quote) {
+                    attrValue += c
+                } else if (c === quote) {
+                    props[attrName] = attrValue
+                    attrName = attrValue = ''
+                    state = 'AttrNameOrJSX'
+                }
+                break
+            case 'SpreadJSX':
+            case 'JSX':
 
-            }
+                var arr = parseCode(string.slice(i), true)
+                i += arr[0].length
+                if (state === 'SpreadJSX') {
+                    props['SpreadJSX'] = arr[1]
+                } else {
+                    props[attrName] = arr[1]
+                }
+                attrName = attrValue = ''
+                state = 'AttrNameOrJSX'
+                console.log(props)
+
+                break
 
         }
-    return [string.slice(0, i), {
-        type: 'props',
-        value: props
-    }]
+
+    }
+
+    throw '必须关闭标签'
 }
